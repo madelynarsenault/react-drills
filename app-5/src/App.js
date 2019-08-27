12ce98582d9d43f28bbc8a0d68240697e702ebbe
Madelyn Arsenault <madelynarsenault@Madelyns-MacBook-Air-2.local>
2019-08-27 16:08:11 -0500

    finals

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Image from "./Image";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+       <Image myImage url={'https://vignette.wikia.nocookie.net/disney/images/9/90/Haku.png/revision/latest?cb=20131201071214'}/>
+       </div>
     );
   }
 }
--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,20 +1,51 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ToDo from './Todo'; 
 
 class App extends Component {
+  constructor(){
+    super()
+
+    this.state ={
+      list: [],
+      input: ""
+    };
+
+    this.handleAddTask = this.handleAddTask.bind(this);
+  }
+  
+  handleInputChange(value){
+    this.setState({input: value});
+  }
+  handleAddTask(){
+    this.setState({
+      list: [...this.state.list, this.setState.input],
+      input: ""
+    });
+  }
+
   render() {
+    let list = this.state.list.map((element, index) => {
+      return <ToDo key={index} task={element} />;
+    });
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>my to-do list that i literally never use</h1>
+      <div>
+      <input value={this.state.input}
+      placeholder="Enter new Task"
+      onChange={e => this.handleInputChange(e.target.value)}
+      />
+
+      <button onClick={this.handleAddTask}>Add Stuff</button>
+      </div> 
+      <br />
+      
+      {list}
       </div>
-    );
+      );
   }
 }
 
