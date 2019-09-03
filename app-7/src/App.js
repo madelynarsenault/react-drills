00d904a0992e26159adfad93713ac342aa44271d
Madelyn Arsenault <madelynarsenault@Madelyns-MacBook-Air-2.local>
2019-09-03 16:44:25 -0500

    finals

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import NewTask from "./NewTask";
-import List from "./list";
+import List from "./List";
 
 class App extends Component {
   constructor () {
